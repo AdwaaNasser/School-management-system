c3e0532f43cd6194af4033bbb2f7043e5e6fedaa
AdwaaNasser <adwanas188@gmail.com>
2024-11-25 14:54:01 +0300

    UnmatchedRangeException.java

---
+++ UnmatchedRangeException.java
@@ -0,0 +1,10 @@
+
+package helloall;
+
+//user defind excption
+public class UnmatchedRangeException extends RuntimeException {
+    
+    public UnmatchedRangeException (){
+        super();
+    }
+}
